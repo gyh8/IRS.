a81123bd7d28b6f6cd2d84d6fa7d1c1a842182ba
Yuanhao Gong <126138571+gyh8@users.noreply.github.com>
2025-02-22 12:44:25 +0800

    Add files via upload

---
+++ irs.m
@@ -0,0 +1,38 @@
+function result = irs(input, lambda, iteration)
+%solve 1/2||U-f||_2^2 + lambda/2*||grad U||_1 
+%implicit residual solver, implemented by Yuanhao Gong
+%please cite the following work if you use this code
+%{ 
+@ARTICLE{irsnet,
+  author={Gong, Yuanhao},
+  journal={IEEE Access}, 
+  title={IRSnet: An Implicit Residual Solver and Its Unfolding Neural Network With 0.003M Parameters for Total Variation Models}, 
+  year={2025},
+  volume={13},
+  number={},
+  pages={10289-10298},
+  doi={10.1109/ACCESS.2025.3528637}}
+%}
+theta = 0.24; 
+threshold = lambda/theta;
+im = single(input);
+bx = zeros(size(im),'single'); 
+by = bx;
+gx=[zeros([size(im,1),1],'single'),diff(im,1,2)];%gradient
+gy=[zeros([1,size(im,2)],'single');diff(im,1,1)];
+for it=1:iteration
+    [tempx,tempy] = myGradDiv(bx,by,theta); 
+    bx = max(min(gx+tempx,threshold),-threshold);%cut
+    by = max(min(gy+tempy,threshold),-threshold);%cut    
+end
+residual=[diff(bx,1,2), -bx(:,end)]+[diff(by,1,1); -by(end,:)];%div
+result = im + theta*residual;
+
+%% gradient of divergence of (gx, gy)
+function [tx,ty]=myGradDiv(gx,gy,theta)
+kx=[1,-2,1]*theta+[0,1,0];
+kxy=[0,1,-1;0,-1,1;0,0,0;]*theta; 
+tx = conv2(gx,kx,'same')+conv2(gy,kxy,'same');
+ty = conv2(gx,kxy','same')+conv2(gy,kx','same');
+tx(:,1)=gx(:,1);%boundary condition
+ty(1,:)=gy(1,:);
